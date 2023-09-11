0cfd0496907c902a4df6183bfe4c31af99eb8c98
Bryan Korff <bryan.korff@warnermedia.com>
2023-09-11 11:51:59 -0400

    Test commit.

--- src/main/kotlin/SeasonsAverage.kt
+++ src/main/kotlin/SeasonsAverage.kt
@@ -12,8 +12,6 @@ data class SeasonsAverageModel(val __typename: String,
                             val awayTeamScore: Int,
                             val stadium: String)
 
-// Comment to modify source code to see if that unblocks PR
-
 class SeasonsAverage {
     fun getTypename(seasonsAverage: String): String {
         val seasonsAverageObj = Json.decodeFromString<SeasonsAverageModel>(seasonsAverage)
